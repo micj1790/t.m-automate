4ed25e5fd4d1161ba0cee2c90bb8992e152e7192
John Michael <micj1790@gmail.com>
2026-05-09 10:01:42 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -26,7 +26,7 @@ export default function About() {
       {/* Hero */}
       <section className="relative py-20 md:py-32 overflow-hidden">
         <div className="absolute inset-0">
-          <img src="https://media.base44.com/images/public/69fefc1890408637f331f461/e1bb194fc_generated_image.png" alt="About T.M Engineering" className="w-full h-full object-cover" />
+          <img src="https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc?w=1920&q=80" alt="About T.M Engineering" className="w-full h-full object-cover" />
           <div className="absolute inset-0 bg-background/88" />
           <div className="absolute inset-0 grid-pattern opacity-40" />
         </div>
