21da9ff08377c7eeece74397bfec1eeb98989ffb
John Michael <micj1790@gmail.com>
2026-05-12 13:03:38 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,8 +13,8 @@ export default function Home() {
       <HeroSection />
       <StatsSection />
       <ServicesPreview />
-      <BrandsBanner />
       <FeaturedProjects />
+      <BrandsBanner />
       <CTASection />
     </>
   );
